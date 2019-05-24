e473ec7e32e9bf69ae713211fded4138c4090b76
hwm09110 <771734186@qq.com>
2019-05-24 11:24:03 +0800

    first-commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// puppeteer
+const puppeteer = require('puppeteer')
+const path = require('path')
+const chalk = require('chalk')
+const log = console.log
+
+
+
+log(chalk.blue('Hello') + ' World' + chalk.red('!'))
+
+
+async function start () {
+	const browser = await puppeteer.launch()
+  	const page = await browser.newPage()
+	await page.goto('http://www.baokaodaxue.com')
+	await page.screenshot({path: 'example.png'})
+
+	await browser.close()
+}
+
+start()
