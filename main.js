d06e037f6e0c301008ef9f6e988d34ff1c72c9e2
hwm09110 <771734186@qq.com>
2019-05-24 16:38:39 +0800

    update

--- main.js
+++ main.js
@@ -6,16 +6,28 @@ const log = console.log
 
 
 
-log(chalk.blue('Hello') + ' World' + chalk.red('!'))
-
-
 async function start () {
-	const browser = await puppeteer.launch()
+	log(chalk.blue('打开浏览器'))
+	const browser = await puppeteer.launch({
+		headless:true //这里我设置成false主要是为了让大家看到效果，设置为true就不会打开浏览器
+	})
   	const page = await browser.newPage()
 	await page.goto('http://www.baokaodaxue.com')
-	await page.screenshot({path: 'bkdx.png'})
+	// const dimensions = await page.evaluate(() => {
+	//     return {
+	//       width: document.documentElement.clientWidth,
+	//       height: document.documentElement.clientHeight,
+	//       deviceScaleFactor: window.devicePixelRatio
+	//     };
+	// });
+	// console.log('Dimensions:', dimensions);
+
+	await page.screenshot({path: 'bkdx.png', fullPage: true})
+	// await page.pdf({path: 'bkdx.pdf', format: 'A4'});  //保存 pdf文件
 
 	await browser.close()
+	log(chalk.blue('关闭浏览器'))
+	process.exit(0);
 }
 
 start()
